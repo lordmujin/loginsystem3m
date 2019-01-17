4afa07b260d9ae4506da0c12223e10e2a9476014
lordmujin <46675490+lordmujin@users.noreply.github.com>
2019-01-17 10:01:42 +0000

    Add files via upload
    Vasco Loubet 3M Nº35

---
+++ header.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>loginsystem</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<div class="main-wrapper">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+				</ul>
+				<div class="nav-login">
+					<form action="includes/login.inc.php" method="POST">
+						<input type="text" name="uid" placeholder="Username/e-mail">
+						<input type="password" name="pwd" placeholder="Password">
+						<button type="submit" name="submit">Login</button>
+					</form>
+					<a href="signup.php">Sign up</a>
+				</div>
+			</div>
+		</nav>
+	</header>
